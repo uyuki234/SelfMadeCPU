973c3b7d5b4766927e5671a07fc002bb6cb3e9de
uyuki234 <208888788+uyuki234@users.noreply.github.com>
2026-02-04 19:28:00 +0900

    NANDゲートからORゲートを作る2

--- verilog_test/Basic.v
+++ verilog_test/Basic.v
@@ -8,46 +8,25 @@ module Basic;
 
     reg r_a;
     reg r_b;
-    reg r_s;
     // --- おまじないここまで ---
 
     wire w_x;
 
-    assign w_x = (r_a & ~r_s) | (r_b & r_s);
+    assign w_x = ~(~(r_a & r_a)) | ~(~(r_b & r_b));
 
     // --- おまじないここから ---
     initial begin
         r_a = 1'b0;
         r_b = 1'b0;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b0;
-        r_b = 1'b0;
-        r_s = 1'b1;
         #2
         r_a = 1'b0;
         r_b = 1'b1;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b0;
-        r_b = 1'b1;
-        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b0;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b1;
-        r_b = 1'b0;
-        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b1;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b1;
-        r_b = 1'b1;
-        r_s = 1'b1;
         #2
         $finish;
     end
