45a0373e4310537ffd3c5da8d4253ec3d3acfa6d
uyuki234 <208888788+uyuki234@users.noreply.github.com>
2026-02-04 17:28:30 +0900

    ORゲート

--- verilog_test/Basic.v
+++ verilog_test/Basic.v
@@ -7,17 +7,26 @@ module Basic;
     end
 
     reg r_a;
+    reg r_b;
     // --- おまじないここまで ---
 
     wire w_x;
 
-    assign w_x = ~r_a;
+    assign w_x = r_a | r_b;
 
     // --- おまじないここから ---
     initial begin
-        r_a = 1'b0;
+        r_a = 1'b0; // 1'b0は2進1ビット
+        r_b = 1'b0; // 'bのbは2進数を表す
         #2
         r_a = 1'b1;
+        r_b = 1'b0;
+        #2
+        r_a = 1'b0;
+        r_b = 1'b1;
+        #2
+        r_a = 1'b1;
+        r_b = 1'b1;
         #2
         $finish;
     end
