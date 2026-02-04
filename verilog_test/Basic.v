a0b5c0c34543cfb77143e5407c41900e345eec84
uyuki234 <208888788+uyuki234@users.noreply.github.com>
2026-02-04 19:32:20 +0900

    三項演算子を用いたマルチプレクサ

--- verilog_test/Basic.v
+++ verilog_test/Basic.v
@@ -8,25 +8,46 @@ module Basic;
 
     reg r_a;
     reg r_b;
+    reg r_s;
     // --- おまじないここまで ---
 
     wire w_x;
 
-    assign w_x = ~(~(r_a & r_a)) | ~(~(r_b & r_b));
+    assign w_x = r_s ? r_b : r_a;
 
     // --- おまじないここから ---
     initial begin
         r_a = 1'b0;
         r_b = 1'b0;
+        r_s = 1'b0;
+        #2
+        r_a = 1'b0;
+        r_b = 1'b0;
+        r_s = 1'b1;
         #2
         r_a = 1'b0;
         r_b = 1'b1;
+        r_s = 1'b0;
+        #2
+        r_a = 1'b0;
+        r_b = 1'b1;
+        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b0;
+        r_s = 1'b0;
+        #2
+        r_a = 1'b1;
+        r_b = 1'b0;
+        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b1;
+        r_s = 1'b0;
+        #2
+        r_a = 1'b1;
+        r_b = 1'b1;
+        r_s = 1'b1;
         #2
         $finish;
     end
