c75e7e536a0a32611a7919703ad310b309135f30
uyuki234 <208888788+uyuki234@users.noreply.github.com>
2026-02-04 19:36:54 +0900

    半加算器

--- verilog_test/Basic.v
+++ verilog_test/Basic.v
@@ -8,46 +8,27 @@ module Basic;
 
     reg r_a;
     reg r_b;
-    reg r_s;
     // --- おまじないここまで ---
 
-    wire w_x;
+    wire w_c;
+    wire w_s;
 
-    assign w_x = r_s ? r_b : r_a;
+    assign w_c = r_a & r_b;
+    assign w_s = r_a ^ r_b;
 
     // --- おまじないここから ---
     initial begin
         r_a = 1'b0;
         r_b = 1'b0;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b0;
-        r_b = 1'b0;
-        r_s = 1'b1;
         #2
         r_a = 1'b0;
         r_b = 1'b1;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b0;
-        r_b = 1'b1;
-        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b0;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b1;
-        r_b = 1'b0;
-        r_s = 1'b1;
         #2
         r_a = 1'b1;
         r_b = 1'b1;
-        r_s = 1'b0;
-        #2
-        r_a = 1'b1;
-        r_b = 1'b1;
-        r_s = 1'b1;
         #2
         $finish;
     end
